05781a5727055948095494001f9cec32c1c08ee9
Puddi1 <101431155+Puddi1@users.noreply.github.com>
2023-08-07 15:28:41 +0200

    barebone is here, creating actual models to understand needs

---
+++ src/huggingface/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ["huggingface"]  # File names to export
+# To add backwards compatbility when importing from other files, simply add:
+# from .fileName import functionName
+from .huggingface import *
